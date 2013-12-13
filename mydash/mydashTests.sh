77e31fc3b1a4da07a0bc9500aab9c83177037c48
Chad Weigle <chadweigle@localhost.localdomain>
2013-12-12 19:08:08 -0700

    Finished fixing mydash for buddy.

--- mydash/mydashTests.sh
+++ mydash/mydashTests.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 sleep 10 &
+echo sleep 10 &
 sleep 10 &
+echo sleep 10 &
 sleep 10 &
+echo sleep 10 &
 
-echo "Each above command should have printed out the command after each execution."
+echo "Each above command has created a sleep process in the background. We will wait for it to finish, then run the jobs command."
 sleep 10
-jobs
 
+jobs
 echo "The above command, jobs, should now have printed out 3 finished sleep commands."
